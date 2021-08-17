8b6856a034216b2a1fbb895f46990782ba8c91d3
unknown <1913674925@qq.com>
2021-08-17 16:53:02 +0800

    revise

--- 数据结构/王道数据结构/4 串/串的顺序存储.c
+++ 数据结构/王道数据结构/4 串/串的顺序存储.c
@@ -23,6 +23,26 @@ bool SubString(SString &Sub,SString S,int pos,int len){
 		Sub.ch[i-pos+1]=S.ch[i];
 	Sub.length =len;
 	return true;
+}
+//比较操作 若S>T则返回值>0;若S=T,则返回值=0 若S<T, 则返回值<0  
+int StrCompare(SString S,SString T)
+{
+	for(int i=1;i<=S.length&&i<=T.length;i++){
+		if(S.ch[i]!=T.ch[i])
+			return S.ch[i]-T.ch[i]; 
+	}
+	//扫描过的所有字符都相同，则长度长的串更大 
+	return S.length-T.length;
+}
+int Index(SString S,SString T){
+	int i=1,n=StrLength(S),m=StrLength(T);
+	SString sub;//用于暂存子串
+	while(i<=n-m+1){
+		SubString(sub,S,i,m);
+		if(StrCompare(sub,T)!=0) ++i;
+		else return i;//返回子串在主串中的位置 
+	}
+	return 0;//S中不存在与T相等的子串 
 } 
 int main()
 {
