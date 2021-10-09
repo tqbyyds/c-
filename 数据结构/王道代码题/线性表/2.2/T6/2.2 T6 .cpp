199a5115650fcb93a3c746c737d10f87b6a71bdd
unknown <1913674925@qq.com>
2021-10-09 11:57:21 +0800

    add

--- 数据结构/王道代码题/线性表/2.2/T6/2.2 T6 .cpp
+++ 数据结构/王道代码题/线性表/2.2/T6/2.2 T6 .cpp
@@ -1,5 +1,10 @@
 #include<stdio.h>
-bool Delete_same(SeqList& L){
+#define MaxSize 50//定义线性表的最大长度 
+typedef struct{
+	int data [MaxSize];//顺序表的元素  
+	int length;//顺序表当前的长度 
+}Sqlist;//顺序表的类型定义
+bool Delete_same(Sqlist &L){
 	if(L.length==0)
 		return false;
 	int i,j; //i存储第一个不相同的元素，j为工作指针 
@@ -11,7 +16,17 @@ bool Delete_same(SeqList& L){
 } 
 int main()
 {
-
+	Sqlist L;
+	L.data[0]=3;
+	L.data[1]=3;
+	L.data[2]=6;
+	L.data[3]=6;
+	L.length=4;
+	Delete_same(L);
+	int i=0;
+	for(i=0;i<L.length;i++){
+		printf("%d\n",L.data[i] );
+	}    
 	return 0;
  }
 
