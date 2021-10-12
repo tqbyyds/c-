ba94e5034d236c3967b71cb8640858eb34851893
unknown <1913674925@qq.com>
2021-10-12 15:20:14 +0800

    add

--- 数据结构/王道数据结构/3 栈和队列/链栈.c
+++ 数据结构/王道数据结构/3 栈和队列/链栈.c
@@ -1,49 +1,36 @@
 #include<stdio.h>
-typedef struct Linknode{
-	int data;
-	struct LinkNode *next;
-} *LStack,LStNode;
-LStack List_HeadInsert(LStack L){ 
-	int x;
-	LStNode *s;
-	L=(LStNode *)malloc(sizeof(LStNode));
-	L->next=NULL;
-	scanf("%d",&x);
-	while(x!=9999){
-		s=(LStNode *)malloc(sizeof(LStNode));
-		s->data=x;
-		s->next=L->next;
-		L->next=s; 
-		scanf("%d",&x);
-	}
-	return L 
-}
-LStNode *GetElem(LStack L,int i){//按位查找 
-		if(i<1)
-			return false;
-		LStNode *p;//指针指向当前扫描到的结点 
-		int j=0;//当前p指向的是第几个结点 
-		p=L;//L指向头节点，头节点是第0个节点（不存储数据） 
-	while(p!=NULL&&j<i-1){//循环找到第i-1个结点 
-		p=p->next;
-		j++;
-	}
-	return p;
-}
-bool LStDelete(LStack L,int i,int *e){
-		if(i<1)
-			return false;
-		LStNode *p=GetElem(L,i-1);//找到第i-1个结点	
-	if(p=NULL)//i值不合法 
-		return false;
-	if(p->next==NULL)//第i-1个节点后已无其他节点 
-		return false;
-	LStNode *q=p->next;//令q指向被删除节点
-	*e=q->data;//用e返回被删元素的值 
-	p->next=q->next; //将*q节点从链中断开
-	free(q);//释放节点存储空间 
-	return true;//删除成功 
+typedef struct StackNode
+{
+	ElemType data;
+	struct StackNode *next;
+ }StackNode,*LinkStack
+ bool InitStack(LinkStack &S){
+ 	//构造一个空栈S，栈顶指针置空
+ 	S=NULL;
+ 	return true;	
+ }
+ bool Push(LinkStack &S,SElemType e){
+ 	//在栈顶插入元素e
+ 	StackNode* p=(StackNode *)malloc(sizeof(StackNode));
+ 	p->data=e;
+ 	p->next=S;
+ 	S=p;
+ 	return true;
+ }
+ bool Pop(LinkStack &S,SElemType &e){
+ 	//删除S的栈顶元素 用e返回其值
+	 if(S==NULL)return false;
+	 e=S->data;//将栈顶元素赋给e
+	 p=S;//用P临时保存栈顶元素空间 以备释放
+	 S=S->next;//修改栈顶指针
+	 free(p);
+	 return true; 
+ }
+SELemType GetTop(LinkStack S){
+	if(S!=NULL)//栈非空
+		return S->data;//返回栈顶元素的值 栈顶指针不变 
 } 
+	 
 int main()
 {
 
